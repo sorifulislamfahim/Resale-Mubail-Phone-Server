1d9b157f0037b63d176017f68b74c70e4dc9e50c
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-11-28 02:02:16 +0600

    connective mongodb and load catagoryID data

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
-const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
+app.use(express.json());
 
-const catagorys = require('./data/catagory.json')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ylc1dr9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.get('/', (req, res) => {
-    res.send('Resail-Mubail-Phone-Server Is Running')
-})
+async function run() {
+    try {
+        const catagorysCollection = client.db('resaleMubail').collection('catagorys')
+        const catagoryOneCollection = client.db('resaleMubail').collection('catagoryOne')
 
-app.get('/catagorys', (req, res) => {
-    res.send(catagorys);
-})
+        app.get('/', (req, res) => {
+            res.send('Resail-Mubail-Phone-Server Is Running')
+        })
+
+        app.get('/catagorys', async (req, res) => {
+            const query = {};
+            const cursor = catagorysCollection.find(query);
+            const catagorys = await cursor.toArray();
+            res.send(catagorys)
+        })
+
+        app.get('/catagorys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {category_id: id}
+            const catagory = await catagoryOneCollection.find(query).toArray();
+            res.send(catagory);
+        })
+
+    }
+    finally {
+       
+    }
+}
+run().catch(error => console.error(error));
+
+
+
+
+
 
 app.listen(port, () => {
     console.log('Resail-Mubail-Phone Rerver is Running Port', port)
 })
+
+
+
+
+
+
+
