c43e6fa73ee07e76c7eeb1ad939ba392bc685c79
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-12-02 12:23:54 +0600

    added secrete token jwt

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json());
